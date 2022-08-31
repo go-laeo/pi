0692b766b7a99603e8ef552be0f1e4bdf839c9ec
laeo <8142123+laeo@users.noreply.github.com>
2022-09-01 00:12:09 +0800

    fix: reworked of Context interface to avoid stack exceeds

--- context.go
+++ context.go
@@ -4,139 +4,66 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"io"
 	"mime/multipart"
+	"net"
 	"net/http"
-	"time"
 )
 
 type Context interface {
-	context.Context
 	http.ResponseWriter
 
-	WithContext(ctx context.Context) Context
+	Context() context.Context
+	SetContext(ctx context.Context)
 
 	Raw() (w http.ResponseWriter, r *http.Request)
 	Query(field string, defaults ...string) string
 	Form(field string, defaults ...string) string
-	File(field string) (multipart.File, *multipart.FileHeader, error)
-	FileSet(field string) []*multipart.FileHeader
-	Cookie(name string) (*http.Cookie, error)
-	HeaderMap() http.Header
-	HeaderValue(name string) string
 
-	Format(v any) error
+	// File returns first uploaded file by field.
+	File(field string) (multipart.File, *multipart.FileHeader, error)
+
+	// FileSet gets all uploaded files by field from underlying request,
+	FileSet(field string) []*multipart.FileHeader
+
+	// Cookie returns the named cookie provided in the request or
+	// ErrNoCookie if not found.
+	// If multiple cookies match the given name, only one cookie will
+	// be returned.
+	Cookie(name string) (*http.Cookie, error)
+
+	// HeaderMap returns http.Header value from underlying request.
+	HeaderMap() http.Header
+
+	// Get gets the first value associated with the given key from request header. If
+	// there are no values associated with the key, Get returns "".
+	// It is case insensitive; textproto.CanonicalMIMEHeaderKey is
+	// used to canonicalize the provided key. Get assumes that all
+	// keys are stored in canonical form. To use non-canonical keys,
+	// access the map directly.
+	Get(name string) string
+
+	// Domain gets domain name of from request's Host field, eg. www.google.com.
+	Domain() string
+
+	IP() string
+	IPSet() []string
+
+	Respond(v any) error
 	Json(v any) error
+	Text(v string) error
 	Redirect(to string, code ...int) error
+	SetCookie(c *http.Cookie)
 }
 
-type ctx struct {
+type _ctx struct {
 	w http.ResponseWriter
 	r *http.Request
 	b *bytes.Buffer
 	c int
 }
 
-var _ Context = (*ctx)(nil)
-
-// Deadline returns the time when work done on behalf of this context
-// should be canceled. Deadline returns ok==false when no deadline is
-// set. Successive calls to Deadline return the same results.
-func (c *ctx) Deadline() (deadline time.Time, ok bool) {
-	return c.r.Context().Deadline()
-}
-
-// Done returns a channel that's closed when work done on behalf of this
-// context should be canceled. Done may return nil if this context can
-// never be canceled. Successive calls to Done return the same value.
-// The close of the Done channel may happen asynchronously,
-// after the cancel function returns.
-//
-// WithCancel arranges for Done to be closed when cancel is called;
-// WithDeadline arranges for Done to be closed when the deadline
-// expires; WithTimeout arranges for Done to be closed when the timeout
-// elapses.
-//
-// Done is provided for use in select statements:
-//
-//	// Stream generates values with DoSomething and sends them to out
-//	// until DoSomething returns an error or ctx.Done is closed.
-//	func Stream(ctx context.Context, out chan<- Value) error {
-//		for {
-//			v, err := DoSomething(ctx)
-//			if err != nil {
-//				return err
-//			}
-//			select {
-//			case <-ctx.Done():
-//				return ctx.Err()
-//			case out <- v:
-//			}
-//		}
-//	}
-//
-// See https://blog.golang.org/pipelines for more examples of how to use
-// a Done channel for cancellation.
-func (c *ctx) Done() <-chan struct{} {
-	return c.r.Context().Done()
-}
-
-// If Done is not yet closed, Err returns nil.
-// If Done is closed, Err returns a non-nil error explaining why:
-// Canceled if the context was canceled
-// or DeadlineExceeded if the context's deadline passed.
-// After Err returns a non-nil error, successive calls to Err return the same error.
-func (c *ctx) Err() error {
-	return c.r.Context().Err()
-}
-
-// Value returns the value associated with this context for key, or nil
-// if no value is associated with key. Successive calls to Value with
-// the same key returns the same result.
-//
-// Use context values only for request-scoped data that transits
-// processes and API boundaries, not for passing optional parameters to
-// functions.
-//
-// A key identifies a specific value in a Context. Functions that wish
-// to store values in Context typically allocate a key in a global
-// variable then use that key as the argument to context.WithValue and
-// Context.Value. A key can be any type that supports equality;
-// packages should define keys as an unexported type to avoid
-// collisions.
-//
-// Packages that define a Context key should provide type-safe accessors
-// for the values stored using that key:
-//
-//	// Package user defines a User type that's stored in Contexts.
-//	package user
-//
-//	import "context"
-//
-//	// User is the type of value stored in the Contexts.
-//	type User struct {...}
-//
-//	// key is an unexported type for keys defined in this package.
-//	// This prevents collisions with keys defined in other packages.
-//	type key int
-//
-//	// userKey is the key for user.User values in Contexts. It is
-//	// unexported; clients use user.NewContext and user.FromContext
-//	// instead of using this key directly.
-//	var userKey key
-//
-//	// NewContext returns a new Context that carries value u.
-//	func NewContext(ctx context.Context, u *User) context.Context {
-//		return context.WithValue(ctx, userKey, u)
-//	}
-//
-//	// FromContext returns the User value stored in ctx, if any.
-//	func FromContext(ctx context.Context) (*User, bool) {
-//		u, ok := ctx.Value(userKey).(*User)
-//		return u, ok
-//	}
-func (c *ctx) Value(key any) any {
-	return c.r.Context().Value(key)
-}
+var _ Context = (*_ctx)(nil)
 
 // Header returns the header map that will be sent by
 // WriteHeader. The Header map also is the mechanism with which
@@ -158,7 +85,7 @@ func (c *ctx) Value(key any) any {
 //
 // To suppress automatic response headers (such as "Date"), set
 // their value to nil.
-func (c *ctx) Header() http.Header {
+func (c *_ctx) Header() http.Header {
 	return c.w.Header()
 }
 
@@ -183,7 +110,7 @@ func (c *ctx) Header() http.Header {
 // writing the response. However, such behavior may not be supported
 // by all HTTP/2 clients. Handlers should read before writing if
 // possible to maximize compatibility.
-func (c *ctx) Write(b []byte) (int, error) {
+func (c *_ctx) Write(b []byte) (int, error) {
 	return c.b.Write(b)
 }
 
@@ -205,20 +132,23 @@ func (c *ctx) Write(b []byte) (int, error) {
 // The server will automatically send a 100 (Continue) header
 // on the first read from the request body if the request has
 // an "Expect: 100-continue" header.
-func (c *ctx) WriteHeader(statusCode int) {
+func (c *_ctx) WriteHeader(statusCode int) {
 	c.c = statusCode
 }
 
-func (c *ctx) WithContext(ctx context.Context) Context {
-	c.r = c.r.WithContext(ctx)
-	return c
+func (c *_ctx) Context() context.Context {
+	return c.r.Context()
 }
 
-func (c *ctx) Raw() (w http.ResponseWriter, r *http.Request) {
+func (c *_ctx) SetContext(ctx context.Context) {
+	c.r = c.r.WithContext(ctx)
+}
+
+func (c *_ctx) Raw() (w http.ResponseWriter, r *http.Request) {
 	return c, c.r
 }
 
-func (c *ctx) Query(field string, defaults ...string) string {
+func (c *_ctx) Query(field string, defaults ...string) string {
 	if c.r.URL.Query().Has(field) {
 		return c.r.URL.Query().Get(field)
 	}
@@ -228,7 +158,7 @@ func (c *ctx) Query(field string, defaults ...string) string {
 	return ""
 }
 
-func (c *ctx) Form(field string, defaults ...string) string {
+func (c *_ctx) Form(field string, defaults ...string) string {
 	v := c.r.FormValue(field)
 	if v != "" {
 		return v
@@ -239,36 +169,73 @@ func (c *ctx) Form(field string, defaults ...string) string {
 	return ""
 }
 
-func (c *ctx) File(field string) (multipart.File, *multipart.FileHeader, error) {
+func (c *_ctx) File(field string) (multipart.File, *multipart.FileHeader, error) {
 	return c.r.FormFile(field)
 }
 
-func (c *ctx) FileSet(field string) []*multipart.FileHeader {
+func (c *_ctx) FileSet(field string) []*multipart.FileHeader {
 	c.r.ParseMultipartForm(1024 * 1024 * 1024)
 	return c.r.MultipartForm.File[field]
 }
 
-func (c *ctx) Cookie(name string) (*http.Cookie, error) {
+func (c *_ctx) Cookie(name string) (*http.Cookie, error) {
 	return c.r.Cookie(name)
 }
 
-func (c *ctx) HeaderMap() http.Header {
+func (c *_ctx) HeaderMap() http.Header {
 	return c.r.Header
 }
 
-func (c *ctx) HeaderValue(name string) string {
+func (c *_ctx) Get(name string) string {
 	return c.r.Header.Get(name)
 }
 
-func (c *ctx) Format(v any) error {
-	panic("not implemented") // TODO: Implement
+func (c *_ctx) Domain() string {
+	return c.r.Host
 }
 
-func (c *ctx) Json(v any) error {
+func (c *_ctx) IP() string {
+	host, _, err := net.SplitHostPort(c.r.RemoteAddr)
+	if err != nil {
+		return ""
+	}
+	return host
+}
+
+func (c *_ctx) IPSet() []string {
+	host, _, err := net.SplitHostPort(c.r.RemoteAddr)
+	if err != nil {
+		return nil
+	}
+	return []string{host}
+}
+
+func (c *_ctx) Respond(v any) (err error) {
+	switch v := v.(type) {
+	case string:
+		err = c.Text(v)
+	case []byte:
+		_, err = c.Write(v)
+	case io.Reader:
+		_, err = io.Copy(c, v)
+	default:
+		err = c.Json(v)
+	}
+	return
+}
+
+func (c *_ctx) Json(v any) error {
+	c.Header().Set("content-type", "application/json")
 	return json.NewEncoder(c).Encode(v)
 }
 
-func (c *ctx) Redirect(to string, code ...int) error {
+func (c *_ctx) Text(v string) error {
+	c.Header().Set("content-type", "text/plain")
+	_, err := c.Write([]byte(v))
+	return err
+}
+
+func (c *_ctx) Redirect(to string, code ...int) error {
 	if len(code) == 0 {
 		http.Redirect(c.w, c.r, to, http.StatusTemporaryRedirect)
 		return nil
@@ -276,4 +243,8 @@ func (c *ctx) Redirect(to string, code ...int) error {
 
 	http.Redirect(c.w, c.r, to, code[0])
 	return nil
+}
+
+func (c *_ctx) SetCookie(co *http.Cookie) {
+	http.SetCookie(c.w, co)
 }
---
+++ context_test.go
@@ -0,0 +1,28 @@
+package ezy
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_ctx_WithContext(t *testing.T) {
+	t.Run("should not throw goroutine stack exceeds", func(t *testing.T) {
+		type a struct{}
+		var _a *a
+		type b struct{}
+		var _b *b
+
+		var c Context = &_ctx{
+			w: httptest.NewRecorder(),
+			r: httptest.NewRequest(http.MethodGet, "/", nil),
+			b: bytes.NewBuffer(nil),
+			c: 200,
+		}
+		c.SetContext(context.WithValue(c.Context(), _a, "a"))
+		c.SetContext(context.WithValue(c.Context(), _b, "b"))
+		c.Context().Value(_a)
+	})
+}
