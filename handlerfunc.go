f900690b3ab01fbf8e3eef4398a3500f399bd910
laeo <8142123+laeo@users.noreply.github.com>
2022-09-14 00:01:59 +0800

    feat: remove generics type param and auto decode body  from HandlerFunc

--- handlerfunc.go
+++ handlerfunc.go
@@ -8,40 +8,16 @@ import (
 	"net/url"
 )
 
-type Void struct{}
+type HandlerFunc func(ctx Context) error
 
-type HandlerFunc[T any] func(ctx Context, p *T) error
-
-func (h HandlerFunc[T]) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	input := new(T)
-	var err error
-
-	if r.ContentLength != 0 {
-		if _, ok := any(input).(*Void); !ok { // skip decode of type *Void
-			err = json.NewDecoder(r.Body).Decode(input)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				json.NewEncoder(w).Encode(NewError(http.StatusBadRequest, err.Error()))
-				return
-			}
-
-			if v, ok := any(input).(Validator); ok {
-				if err = v.Validate(r.Context()); err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-					json.NewEncoder(w).Encode(NewError(http.StatusBadRequest, err.Error()))
-					return
-				}
-			}
-		}
-	}
-
+func (h HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var p url.Values
 	if v := r.Context().Value(&routePathParam{}); v != nil {
 		p = v.(url.Values)
 	}
 
 	c := &_ctx{w: w, r: r, b: &bytes.Buffer{}, c: 200, p: p}
-	if err = h(c, input); err != nil {
+	if err := h(c); err != nil {
 		switch v := err.(type) {
 		case *Error:
 			c.WriteHeader(v.Code)
--- handlerfunc_test.go
+++ handlerfunc_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test_HandlerFuncT_ServeHTTP(t *testing.T) {
-	var fn HandlerFunc[Void] = func(ctx Context, p *Void) error {
+	var fn HandlerFunc = func(ctx Context) error {
 		return NewError(404, "not found")
 	}
 
@@ -22,13 +22,15 @@ func Test_HandlerFuncT_ServeHTTP(t *testing.T) {
 }
 
 func Benchmark_HandlerFuncT_ServeHTTP(b *testing.B) {
-	var fn HandlerFunc[Void] = func(ctx Context, p *Void) error {
+	var fn HandlerFunc = func(ctx Context) error {
 		return NewError(200, "not found")
 	}
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
 
+	b.ReportAllocs()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		fn.ServeHTTP(w, r)
 	}
