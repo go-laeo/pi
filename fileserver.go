37458381ff1ec5b5d507670712584998e3386a86
laeo <8142123+laeo@users.noreply.github.com>
2022-09-14 23:43:03 +0800

    fix: change FileServer to HandlerFunc

--- fileserver.go
+++ fileserver.go
@@ -23,8 +23,10 @@ func openFS(root http.FileSystem, upath string) (http.File, fs.FileInfo, error) 
 
 // FileServer returns a HTTP handler for serving files from within root.
 // If the requested files are not exist, then send defaultsFile to client.
-func FileServer(root http.FileSystem, defaultsFile string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func FileServer(root http.FileSystem, defaultsFile string) HandlerFunc {
+	return func(ctx Context) error {
+		w, r := ctx.Raw()
+
 		upath := r.URL.Path
 		upath = strings.TrimPrefix(upath, "/")
 		upath = path.Clean(upath)
@@ -41,20 +43,20 @@ func FileServer(root http.FileSystem, defaultsFile string) http.Handler {
 			if errors.Is(err, fs.ErrPermission) {
 				w.WriteHeader(http.StatusForbidden)
 				w.Write([]byte("403 Forbidden"))
-				return
+				return nil
 			}
 			if errors.Is(err, fs.ErrNotExist) {
 				w.WriteHeader(http.StatusNotFound)
 				w.Write([]byte("404 page not found"))
-				return
+				return nil
 			}
 
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("500 Internal Server Error"))
-			return
+			return nil
 		}
 
 		http.ServeContent(w, r, path.Base(upath), fi.ModTime(), f)
-		f.Close()
-	})
+		return f.Close()
+	}
 }
