ba5561ba976ca64947676c200e48560f91100a56
laeo <8142123+laeo@users.noreply.github.com>
2022-09-17 13:27:57 +0800

    fix: add omitempty to Result[T any]

--- result.go
+++ result.go
@@ -1,10 +1,10 @@
 package pi
 
 type Result[T any] struct {
-	Data         T      `json:"data"`
-	Error        string `json:"error"`
-	ErrorMessage string `json:"error_message"`
-	Page         int    `json:"page"`
-	PageSize     int    `json:"page_size"`
-	Total        int    `json:"total"`
+	Data         T      `json:"data,omitempty"`
+	Error        string `json:"error,omitempty"`
+	ErrorMessage string `json:"error_message,omitempty"`
+	Page         int    `json:"page,omitempty"`
+	PageSize     int    `json:"page_size,omitempty"`
+	Total        int    `json:"total,omitempty"`
 }
