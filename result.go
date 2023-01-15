06a98ecd650c21552505ca7413848d26a65ddbf1
laeo <8142123+laeo@users.noreply.github.com>
2023-01-15 14:54:16 +0800

    feat: split Result into Result and LengthResult

--- result.go
+++ result.go
@@ -4,7 +4,15 @@ type Result[T any] struct {
 	Data         T      `json:"data,omitempty"`
 	Error        string `json:"error,omitempty"`
 	ErrorMessage string `json:"error_message,omitempty"`
+	OK           bool   `json:"ok"`
+}
+
+type LengthResult[T any] struct {
+	Data         T      `json:"data,omitempty"`
+	Error        string `json:"error,omitempty"`
+	ErrorMessage string `json:"error_message,omitempty"`
 	Page         int    `json:"page,omitempty"`
 	PageSize     int    `json:"page_size,omitempty"`
 	Total        int    `json:"total,omitempty"`
+	OK           bool   `json:"ok"`
 }
